03f3ed0974688f830b23b2012c7d48e8888b55ad
Austin McCartney <amccartney@lanl.gov>
2019-05-07 17:09:52 -0600

    14/17 support toggles

--- cmake/shacl-optional-config.cmake
+++ cmake/shacl-optional-config.cmake
@@ -10,8 +10,8 @@ include("${CMAKE_CURRENT_LIST_DIR}/shacl-optional-targets.cmake")
 find_package(mpark_variant ${shacl-optional_FIND_QUIETLY_ARG})
 if(mpark_variant_FOUND)
   target_link_libraries(optional INTERFACE
-    optional-14
-    mpark_variant)
+    $<$<VERSION_LESS:$<TARGET_PROPERTY:CXX_STANDARD>,17>:shacl::optional-14,mpark_variant>)
 elseif(NOT "${shacl-optional_FIND_QUIETLY_ARG}")
   message(WARNING "shacl::optional C++-14 support requires mpark_variant")
+  set_target_property(shacl::optional INTERFACE_CXX_STANDARD 17)
 endif()
