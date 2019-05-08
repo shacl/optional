4e77a2ea0b9149e71002a98fa318ea6d81065259
Austin McCartney <amccartney@lanl.gov>
2019-05-07 21:58:02 -0600

    more cmake cleanup

--- cmake/shacl-optional-config.cmake
+++ cmake/shacl-optional-config.cmake
@@ -9,9 +9,14 @@ include("${CMAKE_CURRENT_LIST_DIR}/shacl-optional-targets.cmake")
 
 find_package(mpark_variant ${shacl-optional_FIND_QUIETLY_ARG})
 if(mpark_variant_FOUND)
-  target_link_libraries(optional INTERFACE
-    $<$<VERSION_LESS:$<TARGET_PROPERTY:CXX_STANDARD>,17>:shacl::optional-14,mpark_variant>)
-elseif(NOT "${shacl-optional_FIND_QUIETLY_ARG}")
-  message(WARNING "shacl::optional C++-14 support requires mpark_variant")
+  set_target_property(shacl::optional INTERFACE_CXX_STANDARD 14)
+  target_link_libraries(shacl::optional-14 INTERFACE mpark_variant)
+  target_link_libraries(shacl::optional INTERFACE
+    $<$<VERSION_LESS:$<TARGET_PROPERTY:CXX_STANDARD>,17>:shacl::optional-14>)
+
+else()
+  if(NOT "${shacl-optional_FIND_QUIETLY_ARG}")
+    message(WARNING "shacl::optional C++-14 support requires mpark_variant")
+  endif()
   set_target_property(shacl::optional INTERFACE_CXX_STANDARD 17)
 endif()
